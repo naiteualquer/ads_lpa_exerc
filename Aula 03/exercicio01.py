f9c9f621d19b2941e01b55886d869d8bab621415
naiteualquer <valeriogomes@gmail.com>
2022-12-02 12:35:17 -0300

    até exercício dos triângulos

---
+++ Aula 03/exercicio01.py
@@ -0,0 +1,14 @@
+#tipo de triângulo
+
+ladoa = int(input('digite lado a: '))
+ladob = int(input('digite lado b: '))
+ladoc = int(input('digite lado c: '))
+
+if (ladoa and ladob and ladoc) > 0:
+    if ladob <= ladoc and ladoc <= ladob and ladoc <= ladoa:
+
+
+    else:
+            print("um lado não pode ser maior que a soma dos outros dois lados")
+else:
+    print('lada não pode ser zero')
